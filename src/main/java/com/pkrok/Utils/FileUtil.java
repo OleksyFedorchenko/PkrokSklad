b90ba59c34e1988309569c5b9c464563dd3b47e1
OleksyFedorchenko <otihiy@gmail.com>
2018-12-05 23:59:42 +0200

    formatting code

--- src/main/java/com/pkrok/Utils/FileUtil.java
+++ src/main/java/com/pkrok/Utils/FileUtil.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileUtil {
 
     public static Boolean isNotEmpty(MultipartFile multipartFile) {
-        return multipartFile != null && !multipartFile.getOriginalFilename().isEmpty() && multipartFile.getSize() > 0;
+        return multipartFile != null && !multipartFile.getOriginalFilename()
+                .isEmpty() && multipartFile.getSize() > 0;
     }
 }
